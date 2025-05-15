fbaf2f7906cf273b6a6a1859a388c598987f692c
Abdul Razak Abdul Rasheed <67930658+Abdul-Rasheed-coder@users.noreply.github.com>
2025-05-15 02:11:07 +0000

    Add files via upload

---
+++ Asawaba Farms Limited - Website/index.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Asawaba Farms limited</title>
+</head>
+<body>
+	<h1> Asawaba Farms Limited </h1>
+
+</body>
+</html>
